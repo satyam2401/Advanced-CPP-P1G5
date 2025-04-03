3ac1ae31b0a343001b5c0c97cadceba4b86db238
joecooney-UC <joecooney@uchicago.edu>
2025-04-03 12:05:25 -0500

    add returns

--- Phase 1/MatrixOps.cpp
+++ Phase 1/MatrixOps.cpp
@@ -23,10 +23,11 @@ void MatrixOps::multiply_mv_row_major(const double *matrix, int rows, int cols, 
         }
         result[row] = temp;
     }
+    return;
 }
 
 void MatrixOps::multiply_mv_col_major(const double *matrix, int rows, int cols, const double *vector, double *result) {
-
+    return;
 }
 
 void MatrixOps::multiply_mm_naive(const double *matrixA, int rowsA, int colsA, const double *matrixB, int rowsB, int colsB,
@@ -64,5 +65,5 @@ void MatrixOps::multiply_mm_naive(const double *matrixA, int rowsA, int colsA, c
 
 void MatrixOps::multiply_mm_transposed_b(const double *matrixA, int rowsA, int colsA, const double *matrixB_transposed,
                                          int rowsB, int colsB, double *result) {
-
+    return;
 }
