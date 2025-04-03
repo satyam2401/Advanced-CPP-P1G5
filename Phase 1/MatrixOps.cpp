6635b0d886f31d6e4d6a1f5c6e66d607d5646ba6
joecooney-UC <joecooney@uchicago.edu>
2025-04-03 11:23:36 -0500

    mm_naive_implemented

--- Phase 1/MatrixOps.cpp
+++ Phase 1/MatrixOps.cpp
@@ -29,10 +29,36 @@ void MatrixOps::multiply_mv_col_major(const double *matrix, int rows, int cols, 
 
 }
 
-void
-MatrixOps::multiply_mm_naive(const double *matrixA, int rowsA, int colsA, const double *matrixB, int rowsB, int colsB,
+void MatrixOps::multiply_mm_naive(const double *matrixA, int rowsA, int colsA, const double *matrixB, int rowsB, int colsB,
                              double *result) {
+        if (!matrixA || !matrixB || !result) {
+            std::cerr << "[ERROR] Null pointer passed to multiply_mm_naive.\n";
+            return;
+        }
+                            
+        if (rowsA <= 0 || colsA <= 0 || rowsB <= 0 || colsB <=0) {
+            std::cerr << "[ERROR] Invalid matrix dimensions.\n";
+            return;
+        }
 
+        if (colsA != rowsB) {
+            std::cerr << "[ERROR] Invalid Matrix dimensions.\n";
+            return;
+        }
+
+        // each row of A gets dotted with the corresponding col of B (do not transpose B or make a new one)
+        for (int rowA = 0; rowA < rowsA; ++rowA){
+            for (int colB = 0; colB < colsB; ++colB){
+                double dotResult = 0.0;
+                for (int colA = 0; colA < colsA; ++colA){
+                    dotResult += matrixA[colsA * rowA + colA] * matrixB[colsB * colA + colB];
+                    // whoever is doing the next function, code is identical. Switch this line with:
+                    // dotResult += matrixA[colsA * rowA + colA] * matrixB[colsB * colB + colA];
+                }
+                result[colsB * rowA + colB] = dotResult;
+            }
+        }
+        
 }
 
 void MatrixOps::multiply_mm_transposed_b(const double *matrixA, int rowsA, int colsA, const double *matrixB_transposed,
