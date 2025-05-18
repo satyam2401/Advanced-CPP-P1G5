aa03b21784a4ab0478be843cbba9f901bf9e9506
xitaram <xitaaz@uchicago.edu>
2025-05-18 18:39:05 -0400

    Amended duplicate order structs

--- Session7/OrderTypes.hpp
+++ Session7/OrderTypes.hpp
@@ -12,18 +12,3 @@ struct Order {
     std::chrono::high_resolution_clock::time_point enqueueTime;
     std::chrono::high_resolution_clock::time_point dequeueTime;
 };
-// OrderTypes.hpp
-#pragma once
-#include <string>
-#include <chrono>
-
-// Define a simple Order struct
-struct Order {
-    int orderId;
-    std::string symbol;
-    int quantity;
-    double price;
-    bool isBuy;
-    std::chrono::high_resolution_clock::time_point enqueueTime;
-    std::chrono::high_resolution_clock::time_point dequeueTime;
-};
